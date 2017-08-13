6b9860d424644e4bbda782def503afe45d625c10
Sélim Chiki <selimchiki@yahoo.fr>
2017-08-13 23:01:23 +0200

    Add Password Reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813201410) do
+ActiveRecord::Schema.define(version: 20170813205141) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'bool' for column 'activated'
